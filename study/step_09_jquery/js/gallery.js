42e0692fea6ca4cbd998e7bac0be1ebb39af9639
erivery2 <erivery2@gmail.com>
2016-05-27 09:16:43 +0900

    gallery_자막포함_최종

--- study/step_09_jquery/js/gallery.js
+++ study/step_09_jquery/js/gallery.js
@@ -3,14 +3,24 @@ function gallery(){
 			gallerybox = $('#gallery'),
 			gallerybtn = gallerybox.find('a');
 
-	imgbox.html("<img>");
+	imgbox.html("<img src='./img/test.jpg'>").find('img')
+				.css({'width': 100+'%', 'height': 'auto'});
+
+	imgbox.append('<span>');
 	gallerybtn.on('click', function(e){
 		e.preventDefault();
-		var data_img = $(this).attr("data-img"),
-				a_nar = $(this).text();
-		// imgbox.html("<img>").attr({'src','alt'});
 		
-		imgbox.find('img').css({'width': 100+'%', 'height': 'auto'})
+		var imgspan = imgbox.find('span'),
+				data_img = $(this).attr("data-img"),
+				a_nar = $(this).text();
+
+		imgbox.find('img')
 					.attr({'src': data_img, 'alt':a_nar});
+		// imgbox.append('<span>');
+		// imgbox.find('span').css({'bottom': '-100px'}).text(a_nar).animate({'bottom':0});
+
+		imgbox.find('span').animate({'bottom': '-100px'},300, function(){
+			imgspan.text(a_nar).animate({'bottom':0},500);
+		});
 	});
 }; // gallery
