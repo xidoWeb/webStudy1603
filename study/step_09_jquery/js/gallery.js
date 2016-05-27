5501e0ce4b0e5b375eceb294435800ad9bc9c2ab
erivery2 <erivery2@gmail.com>
2016-05-27 13:10:49 +0900

    button_gallery_slide

--- study/step_09_jquery/js/gallery.js
+++ study/step_09_jquery/js/gallery.js
@@ -1,5 +1,5 @@
 function gallery(){
-	var imgbox = $('.box'),
+	var imgbox     = $('.box'),
 			gallerybox = $('#gallery'),
 			gallerybtn = gallerybox.find('a');
 
@@ -10,22 +10,49 @@ function gallery(){
 	imgbox.append('<div>');
 
 	// 좌/우 버튼 클릭
-var gal_btn    = $('gall_btn'),
-		gal_left	= $('gallery_btn_left'),
-		gal_right	= $('gallery_btn_right'),
-		thum_box = $('.thumbox'),
-		thum_box_ul = thum_box.children('ul'),
-		thum_box_li = thum_box_ul.find('li'),
-		thum_box_li_w = thum_box_li.width(thum_box.width() / 10),
-		thum_box_li_set = thum_box_li.length,
-		thum_box_w = parseInt(thum_box_li_w * thum_box_li_set );
+	var gal_btn           = $('.gall_btn'),
+			gal_left	        = $('.gallery_btn_left'),
+			gal_right	        = $('.gallery_btn_right'),
+			thum_box          = $('.thumbox'),
+			thum_box_w        = $('.thumbox').width(),
+			thum_box_ul       = thum_box.children('ul'),
+			list_length	      =	5, // li크기값 및 반복처리할 갯수 설정
+			thum_box_li       = thum_box_ul.find('li'),
+			widbox = Math.round(thum_box_w / list_length);
+			thum_box_li_w     = thum_box_li.width( widbox ), 
+			thum_box_li_width = thum_box_li.width(), // li크기값 가져오기(설정한 값을 가져와서 ul의 크기값 배정)
+			thum_box_li_set   = thum_box_li.length, // thumnail 이미지 보이는 갯수파악하기
+			thum_box_ul_width = thum_box_li_width * thum_box_li_set *2;
+			move_length	      = thum_box_li.eq(list_length);
 
-		console.log(thum_box_li_w);
-		console.log(thum_box_w);
-		thum_box_ul.width(thum_box_w);
-		gal_btn.on('click', function(){
+	//-------------------------------------------
+	thum_box_li.last().prependTo(thum_box_ul);
 
-		});
+	thum_box_ul.width(thum_box_ul_width);
+	thum_box_ul.css({'margin-left':-thum_box_li_width});
+
+	var gulWidth = thum_box_ul.width(thum_box_ul_width);
+	
+	gal_left.on('click', function(){
+			var ul_margin = thum_box_ul.css('margin-left'),
+					thum_box_li       = thum_box_ul.find('li');
+
+			thum_box_li.first().stop().delay(150).animate({'margin-left':'+='+thum_box_li_width},500, function(){
+				thum_box_li.css({'margin':0});
+				thum_box_li.last().prependTo(thum_box_ul);
+			})
+	});// gal_left.click
+
+	gal_right.on('click', function(){
+		var ul_margin = thum_box_ul.css('margin-left'),
+					thum_box_li       = thum_box_ul.find('li');
+
+				thum_box_li.first().stop().delay(150).animate({'margin-left':'-='+thum_box_li_width},500, function(){
+					thum_box_li.css({'margin':0});
+					thum_box_li.first().appendTo(thum_box_ul);
+			})
+
+	});// gal_left.click
 
 
 	// 이미지 클릭
@@ -41,12 +68,9 @@ var gal_btn    = $('gall_btn'),
 		imgbox.find('img')
 					.attr({'src': data_img, 'alt':a_nar});
 
-		//자막 내용 보이기(애니메이션 설정)			
-		// imgbox.append('<span>');
-		// imgbox.find('span').css({'bottom': '-100px'}).text(a_nar).animate({'bottom':0});
-
 		imgbox.find('div').stop().animate({'bottom': '-100px'},300, function(){
 			imgspan.html(a_el).animate({'bottom':0},500);
 		});
-	});
+
+	}); // gallerybtn.click
 }; // gallery
